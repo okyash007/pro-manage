801763f82762414fa9401ed314d112ddb137dbf9
Yash <yash.verma@tailboost.ai>
2024-07-23 18:24:33 +0530

    add server to lambda

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,12 @@
 import { app } from "./app.js";
 import dotenv from "dotenv";
 import connectDb from "./db/connectDb.js";
+import serverless from "serverless-http";
 
 dotenv.config({
   path: "./.env",
 });
 
-connectDb().then(() => {
-  app.listen(process.env.PORT, () => {
-    console.log(`⚙️  Server is running at port : ${process.env.PORT} `);
-  });
-});
+connectDb()
+
+export const handler = serverless(app);
--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,17 +1,7 @@
-export const backendUrl = "https://pro-manage-lb3p.onrender.com";
+export const backendUrl =
+  "https://jfmimf24cxadtdjog2oyt7jfp40wvwze.lambda-url.ap-south-1.on.aws";
 export const frontendUrl = "https://pro-manage-yash.vercel.app";
 export const sections = ["backlog", "toDo", "inProgress", "done"];
-
-
-
-
-
-
-
-
-
-
-
 
 // https://promanage-5ug9.onrender.com
 // http://localhost:2000
